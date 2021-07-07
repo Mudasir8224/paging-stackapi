524ac7fef5b3544f6f6d7a3ad07edb0ff82167b2
mudasirBhatti <mohammad.mudasir@transdata.biz>
2021-07-07 11:24:17 +0500

    make api response pojo

---
+++ app/src/main/java/com/example/pagingdemo/Api.java
@@ -0,0 +1,16 @@
+package com.example.pagingdemo;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface Api {
+
+    @GET("answers")
+    Call<StackApiResponse> getAnswer(
+            @Query("page") int page,
+            @Query("pagesize") int size,
+            @Query("site") String site
+    );
+
+}
